b98b257817f7f08ddd4b2d8d060c43617f7dfa81
Shlomi Moreh <shlomi295@gmail.com>
2020-03-31 22:30:05 +1100

    added curl setopt

--- create.php
+++ create.php
@@ -4,9 +4,12 @@ $url = "https://personal-sites.deakin.edu.au/~smoreh/sit780/api/create.php";
 
 $handle = curl_init();
 curl_setopt($handle,CURLOPT_URL,$url);
+curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_POST, 1);
 $output = curl_exec($handle);
 curl_close($handle);
+
 $result= json_decode($output, true);
 
 print "<!DOCTYPE html> <html><head><title>cURL</title></head><body>";
