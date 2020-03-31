5cef45b7d63fe595cec3184e7e18462078e5430c
Shlomi Moreh <shlomi295@gmail.com>
2020-03-31 22:21:49 +1100

    Update create.php

--- create.php
+++ create.php
@@ -4,5 +4,13 @@ $url = "https://personal-sites.deakin.edu.au/~smoreh/sit780/api/create.php";
 
 $handle = curl_init();
 curl_setopt($handle,CURLOPT_URL,$url);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true)
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$output = curl_exec($handle);
+curl_close($handle);
+$result= json_decode($output, true);
+
+print "<!DOCTYPE html> <html><head><title>cURL</title></head><body>";
+print "<table border='1' width='30%'>";
+
+
 ?>
