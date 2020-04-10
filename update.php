0d8dd0153d44f5ba56526f2f2ea534bd74b1e162
Shlomi Moreh <shlomi295@gmail.com>
2020-04-10 13:56:41 +1000

    more style of main form

--- update.php
+++ update.php
@@ -11,8 +11,12 @@ $q = $_REQUEST['q'];
 $output = callAPI("PUT", $url, $q);
 $result = json_decode($output, true);
 
-print "<!DOCTYPE html> <html><head><title>cURL</title></head><body>";
+print "<!DOCTYPE html> <html><head><title>cURL</title>
+<link rel = 'stylesheet'
+   type = 'text/css'
+   href = 'Style.css' /></head><body>";
 print  "<form action='create.php' method='post'>";
+print "<h2>Update User</h2>";
 print "<table><tr>";
 print "<td>ID</td><td><input type='text' value=".$result['id']." name='id' id='sid'></td>";
 print "</tr><tr>";
